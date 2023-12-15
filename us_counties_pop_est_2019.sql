8970595f0c91ddf52599cfbf6cde7733e03c70c6
Brandy Wilkinson <131201548+brandywii@users.noreply.github.com>
2023-12-15 17:30:36 -0500

    Add files via upload
    This file contains the code for the new table called us_counties_pop_est_2019 within the database named analysis.

---
+++ us_counties_pop_est_2019.sql
@@ -0,0 +1,23 @@
+/*Create a neww table called 'us_counties_pop_est_2019' 
+and create the column names that will be imported from the csv file */
+
+CREATE TABLE us_counties_pop_est_2019 (
+	state_fips text,
+	county_fips text,
+	region smallint,
+	state_name text,
+	county_name text,
+	area_land bigint,
+	area_water bigint,
+	internal_point_lat numeric(10,7),
+	internal_point_lon numeric(10,7),
+	pop_est_2018 integer,
+	pop_est_2019 integer,
+	births_2019 integer,
+	deaths_2019 integer,
+	international_migr_2019 integer,
+	domestic_migr_2019 integer,
+	residual_2019 integer,
+	CONSTRAINT counties_2019_key PRIMARY KEY (state_fips,county_fips)
+	);
+
